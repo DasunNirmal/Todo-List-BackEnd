89461978a51440ee91fb2ee401db64518b71d72f
DasunNirmal <dasun200301@gmail.com>
2025-04-09 21:16:03 +0530

    save done

--- src/main/java/com/example/todolistbackend/service/TaskService.java
+++ src/main/java/com/example/todolistbackend/service/TaskService.java
@@ -1,4 +1,7 @@
 package com.example.todolistbackend.service;
 
+import com.example.todolistbackend.dto.impl.TaskDtoImpl;
+
 public interface TaskService {
+    void saveTask(TaskDtoImpl taskDto);
 }
