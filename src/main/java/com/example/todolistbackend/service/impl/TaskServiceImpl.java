5ee1df5ca474d46ca4f5531edf9ab38113227b01
DasunNirmal <dasun200301@gmail.com>
2025-04-09 21:16:14 +0530

    save done

--- src/main/java/com/example/todolistbackend/service/impl/TaskServiceImpl.java
+++ src/main/java/com/example/todolistbackend/service/impl/TaskServiceImpl.java
@@ -1,8 +1,28 @@
 package com.example.todolistbackend.service.impl;
 
+import com.example.todolistbackend.dao.TaskDao;
+import com.example.todolistbackend.dto.impl.TaskDtoImpl;
+import com.example.todolistbackend.entity.impl.TaskEntity;
+import com.example.todolistbackend.exeption.DataPersistException;
 import com.example.todolistbackend.service.TaskService;
+import com.example.todolistbackend.utill.AppUtil;
+import com.example.todolistbackend.utill.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskServiceImpl implements TaskService {
+    @Autowired
+    private TaskDao taskDao;
+    @Autowired
+    private Mapping mapping;
+
+    @Override
+    public void saveTask(TaskDtoImpl taskDto) {
+        taskDto.setId(AppUtil.generateTaskId());
+        TaskEntity save = taskDao.save(mapping.toTaskEntity(taskDto));
+        if (save == null) {
+            throw new DataPersistException("Task not saved");
+        }
+    }
 }
