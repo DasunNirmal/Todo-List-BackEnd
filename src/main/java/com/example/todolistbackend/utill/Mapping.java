a649a8db76f600d68d4a7613a0dfd166b1519ed2
DasunNirmal <dasun200301@gmail.com>
2025-04-09 21:15:08 +0530

    mappings for tasks

--- src/main/java/com/example/todolistbackend/utill/Mapping.java
+++ src/main/java/com/example/todolistbackend/utill/Mapping.java
@@ -1,11 +1,26 @@
 package com.example.todolistbackend.utill;
 
+import com.example.todolistbackend.dto.impl.TaskDtoImpl;
+import com.example.todolistbackend.entity.impl.TaskEntity;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.modelmapper.ModelMapper;
+
+import java.util.List;
 
 @Component
 public class Mapping {
     @Autowired
     private ModelMapper modelMapper;
+
+    public TaskEntity toTaskEntity(TaskDtoImpl taskDto) {
+        return modelMapper.map(taskDto, TaskEntity.class);
+    }
+    public TaskDtoImpl toTaskDto(TaskEntity taskEntity) {
+        return modelMapper.map(taskEntity, TaskDtoImpl.class);
+    }
+    public List<TaskDtoImpl> toTaskDtoList(List<TaskEntity> taskEntityList) {
+        return modelMapper.map(taskEntityList, new TypeToken<List<TaskDtoImpl>>() {}.getType());
+    }
 }
