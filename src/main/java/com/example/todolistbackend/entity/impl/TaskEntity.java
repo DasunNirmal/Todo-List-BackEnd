052ccb46f1487d798a49d8ed52f21036b172c49e
DasunNirmal <dasun200301@gmail.com>
2025-04-09 20:30:39 +0530

    entity tasks

---
+++ src/main/java/com/example/todolistbackend/entity/impl/TaskEntity.java
@@ -0,0 +1,23 @@
+package com.example.todolistbackend.entity.impl;
+
+import com.example.todolistbackend.entity.SuperEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tasks")
+public class TaskEntity implements SuperEntity {
+    @Id
+    private int id;
+    private String title;
+    private String description;
+    private String status;
+    private String createdAt;
+}
