a7b0362aa8359f6191cd3b3321479f3e25decab6
DasunNirmal <dasun200301@gmail.com>
2025-04-09 21:14:48 +0530

    exception

---
+++ src/main/java/com/example/todolistbackend/exeption/DataPersistException.java
@@ -0,0 +1,14 @@
+package com.example.todolistbackend.exeption;
+
+public class DataPersistException extends RuntimeException {
+
+    public DataPersistException(String message) {
+
+    }
+    public DataPersistException(String message, Throwable cause) {
+
+    }
+    public DataPersistException(Throwable cause) {
+
+    }
+}
