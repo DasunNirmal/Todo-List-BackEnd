482cf6ca84ae6a1d7441ceb1e1b05dc63f64d1ed
DasunNirmal <dasun200301@gmail.com>
2025-04-09 20:29:01 +0530

    Model Mapper defined as a bean

--- src/main/java/com/example/todolistbackend/TodoListBackEndApplication.java
+++ src/main/java/com/example/todolistbackend/TodoListBackEndApplication.java
@@ -1,7 +1,9 @@
 package com.example.todolistbackend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoListBackEndApplication {
@@ -9,5 +11,7 @@ public class TodoListBackEndApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoListBackEndApplication.class, args);
     }
+    @Bean
+    public ModelMapper modelMapper() {return new ModelMapper();}
 
 }
