914434d9b360cfce2f4b28d94d7320577036777a
DasunNirmal <dasun200301@gmail.com>
2025-04-09 21:15:22 +0530

    save done and working

--- src/main/java/com/example/todolistbackend/controller/TaskController.java
+++ src/main/java/com/example/todolistbackend/controller/TaskController.java
@@ -1,4 +1,28 @@
 package com.example.todolistbackend.controller;
 
+import com.example.todolistbackend.dto.impl.TaskDtoImpl;
+import com.example.todolistbackend.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:4200/")
+@RestController
+@RequestMapping("api/v1/tasks")
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveTasks(@RequestBody TaskDtoImpl taskDto) {
+        try {
+            taskService.saveTask(taskDto);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
